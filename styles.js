afeb9bc874b506dbb7f4a27b57f884c1ff399742
olufunke owoseni <owoseniolufunke1011@gmail.com>
2020-10-20 15:15:21 +0100

    adding js file

--- styles.js
+++ styles.js
@@ -1,5 +1,5 @@
 function submitInfo(); {
-    // const contact = document.querySelector('.form');
+    
     var Name = '';
     var email = '';
     var message = '';
@@ -8,12 +8,10 @@ function submitInfo(); {
     Name = document.getElementById('Name').value;
     email = document.getElementById('emailAddress').value;
     message = document.getElementById('message').value;
-    // console.log('Your name ' + Name);
-    // console.log('Email Address ' + email);
-    // console.log('Your Message Here... ' + message);
-if (Name == '' && email == '' && message == '') {
-  alert('please input missing field');
-    // console.log(contact);
+
+if (Name == '' || email == '' || message == '') {
+  alert('please input missing field')
+   
 } else {
     alert('Your response has been submit, you will recieve an email shortly');
 
@@ -37,23 +35,3 @@ if (Name == '' && email == '' && message == '') {
 
 
 
-// function submitInfo() {
-//     var firstName = '';
-//     var lastName = '';
-//     var email = '';
-//     var password = '';
-
-//     firstName = document.getElementById('firstName').value;
-//     lastName = document.getElementById('lastName').value;
-//     email = document.getElementById('email').value;
-//     password = document.getElementById('password').value;
-//     console.log('first Name:' + firstName);
-//     console.log('last Name:' + lastName);
-//     console.log('email:' + email);
-//     console.log('Password:' + password);
-// if (firstName == '' && lastName == '' && email == '' && password == '') {
-//     alert ('input missing field');
-// } else {
-//     alert ('Form has been submitted');
-// }
-// }
